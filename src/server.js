5436b0846b4b122398563bf9b6cbbda937bdf3bb
Tariq Chang <tariqhussainchang363@gmail.com>
2023-08-24 07:52:06 +0500

    fixed cors error

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const passport = require('passport');
 const authRoutes = require('./routes/authRoutes')
 const blogRoutes = require('./routes/blogRoutes');
+const path = require('path');
+const cors = require('cors');
 const intializePassport = require('./config/passport');
+
 
 require('dotenv').config();
 
@@ -14,6 +17,8 @@ intializePassport(passport);
 require('./config/db');
 
 const app = express();
+app.use(cors());
+app.use('/media', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
@@ -22,9 +27,10 @@ app.get('/api/v1',passport.authenticate('jwt', {session: false}), (req,res) => {
     res.send("Homepage");
 })
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.redirect('/api/v1')
 })
+
 
 
 const PORT = process.env.PORT || 4000;
