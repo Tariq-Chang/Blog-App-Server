5e5a705ba82c622a7adc5f359dbb2f6b01b65e30
Tariq Hussain <tariqhussainchang363@gmail.com>
2024-01-21 11:45:36 +0500

    updated multer config file

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,15 +1,22 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-const upload = multer({
-    storage:multer.diskStorage({
-        destination: function(req, file, cb) {
-          cb(null, path.join(__dirname,'..','uploads'));
-        },
-        filename: function(req, file, cb) {
-          cb(null, new Date().toISOString() + file.originalname);
-        }
-      })
-})
+const uploadDirectory = path.join(__dirname, "..", "uploads");
 
+// Create the uploads directory if it doesn't exist
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDirectory); // Adjusted destination path
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 module.exports = upload;
