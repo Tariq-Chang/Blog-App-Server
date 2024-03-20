03fecc07e722e4c2d9a8c28a1bd65de22d5fb2cf
Tariq Chang <tariqhussainchang363@gmail.com>
2024-03-20 07:49:23 -0700

    added get comments for blog api's

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,6 +1,30 @@
 const Blog = require('../models/blogSchema');
 const Comment = require('../models/commentSchema');
 
+// ********************************** GET ALL COMMENTS ***********************************
+const getComments = async (req, res) => {
+    const {blogId} = req.params;
+    try {
+        const blog = await Blog.findOne({_id: blogId})
+        const commentIds = blog.comments;
+        const comments = await Comment.find({_id: {$in: commentIds}}).populate('user')
+        res.status(200).json({comments});
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
+// ********************************** GET COMMENT ****************************************
+const getComment = async (req, res) => {
+    const {commentId} = req.params;
+    try {
+        const comment = await Comment.findOne({_id: commentId}).populate('user');
+        res.status(200).json({comment})
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
 // ********************************** ADD COMMENT ****************************************
 const addComment = async (req, res) => {
     const { comment } = req.body;
@@ -78,4 +102,4 @@ const updateComment = async(req,res) => {
         return res.status(500).json({ error: error.message });
     }
 }
-module.exports = { addComment, deleteComment, updateComment };
+module.exports = {getComments, getComment, addComment, deleteComment, updateComment };
--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { isAuthorizedUser } = require('../middleware/auth');
 const {deleteBlog, createBlog, getUserBlogs, updateBlog, searchBlogByTitle, getAllBlogs, updateUserInfo, addBlogThumbnail, addBlogImages, saveBlog, getSavedBlogs, removeSavedBlog, getBlog, incrementLikes, decrementLikes } = require('../controllers/blogController');
-const { addComment, deleteComment, updateComment } = require('../controllers/commentController');
+const { addComment, deleteComment, updateComment, getComments, getComment } = require('../controllers/commentController');
 const upload = require('../middleware/upload');
 
 const router = express.Router();
@@ -12,9 +12,6 @@ router.get('/:blogId', getBlog)
 router.post('/create',upload.single('profile'), createBlog);
 router.delete('/delete/:id', isAuthorizedUser(['admin', 'author']), deleteBlog)
 router.put('/update/:id', isAuthorizedUser(['admin', 'author']), updateBlog)
-router.post('/:blogId/comment/add', addComment);
-router.delete('/:blogId/comment/:commentId/delete',isAuthorizedUser(['admin', 'author']), deleteComment);
-router.put('/comment/:commentId/update',isAuthorizedUser(['admin', 'author']), updateComment);
 router.get('/search', searchBlogByTitle)
 router.put('/updateUserInfo',updateUserInfo);
 router.post('/addBlogThumbnail',isAuthorizedUser(['admin', 'author']), upload.single('thumbnail'), addBlogThumbnail);
@@ -23,5 +20,10 @@ router.put('/saveBlog', saveBlog);
 router.delete('/removeSavedBlog/:id', removeSavedBlog);
 router.patch('/:blogId/incrementLikes', incrementLikes);
 router.patch('/:blogId/decrementLikes', decrementLikes);
+router.get('/:blogId/comments/', getComments);
+router.get('/comments/:commentId',getComment);
+router.post('/:blogId/comment/add', addComment);
+router.delete('/:blogId/comment/:commentId/delete',isAuthorizedUser(['admin', 'author']), deleteComment);
+router.put('/comment/:commentId/update',isAuthorizedUser(['admin', 'author']), updateComment);
 
 module.exports = router;
